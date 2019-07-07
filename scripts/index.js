61480ce9e72b9c3f4fc87e02c2421351f3ee7340
Jackie Symons <jackiedsymons@gmail.com>
2019-07-06 23:56:41 -0400

    Updates to details.

--- scripts/index.js
+++ scripts/index.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 
   function calculate () {
     var $displayValue = $('.display-value');
-    var num1Calculated = parseFloat(num1);
-    var num2Calculated = parseFloat(num2);
-    var op = num1Calculated + operator + num2Calculated;
+    var num1Calc = parseFloat(num1);
+    var num2Calc = parseFloat(num2);
+    var op = num1Calc + operator + num2Calc;
     var total = eval(op);
 
     if (total % 1 !== 0) {
@@ -27,7 +27,7 @@ $(document).ready(function() {
       $displayValue.text('oops! too high!');
     } else {
       $displayValue.text(total);
-    }
+    };
 
     num1 = total;
   };
@@ -59,19 +59,16 @@ $(document).ready(function() {
     if (num1.length === 0) {
       num1 = '0';
       operator = $buttonValue;
-      console.log(num1,operator,num2,reset);
     } else if (operator.length === 0) {
       operator = $buttonValue;
-      console.log(num1,operator,num2,reset);
-    } else if ((operator.length === 1) && (reset === false) && (num2.length > 0)) {
+    } else if ((operator.length === 1) && (reset === false)
+    && (num2.length > 0)) {
       calculate ();
       operator  = $buttonValue;
       num2 = '';
-      console.log(num1,operator,num2,reset);
     } else if (operator.length === 1) {
       num2 = '';
       operator = $buttonValue;
-      console.log(num1,operator,num2,reset);
     }
   });
 
@@ -89,6 +86,5 @@ $(document).ready(function() {
     num1 = '';
     num2 = '';
     operator = '';
-    console.log(num1,operator,num2,reset);
   });
 });
